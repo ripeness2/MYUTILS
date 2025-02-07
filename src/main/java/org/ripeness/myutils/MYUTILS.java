1cbcd2402f1342059fbfc9f10f51777ef1f12929
DOOMRise1 <fananvsp@gmail.com>
2025-02-07 05:01:59 +0300

    pom change

--- src/main/java/org/ripeness/myutils/MYUTILS.java
+++ src/main/java/org/ripeness/myutils/MYUTILS.java
@@ -2,16 +2,5 @@ package org.ripeness.myutils;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MYUTILS extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+public final class MYUTILS {
 }
