990363a4e7b0f5a7a24aa84fb93e618d7f81c5b4
DOOMRise1 <fananvsp@gmail.com>
2025-05-19 04:14:47 +0300

    pom change

--- src/main/java/org/ripeness/myutils/utils/textutil/Lang.java
+++ src/main/java/org/ripeness/myutils/utils/textutil/Lang.java
@@ -3,6 +3,8 @@ package org.ripeness.myutils.utils.textutil;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Lang {
 
@@ -16,7 +18,11 @@ public class Lang {
     // getText method
     public listed getText(String cfloc) {
         if (config.contains(cfloc)) {
-            return new listed(config.getStringList(cfloc));
+            List<String> stringList = config.getStringList(cfloc);
+            if (!config.isList(cfloc) && config.isSet(cfloc)) {
+                stringList = Collections.singletonList(config.getString(cfloc));
+            }
+            return new listed(stringList);
         }
         return new listed(new ArrayList<>());
     }
